913c8758e42adcbbd6dd0e434f9aace9c731f19c
Akanksha101999 <97754410+Akanksha101999@users.noreply.github.com>
2022-01-16 23:41:06 +0530

    Add files via upload

---
+++ git11Questions/AccessMod2.java
@@ -0,0 +1,16 @@
+package git11Questions;
+  class AccessMod2 { 
+ protected void show() {
+	System.out.println("We can access Protected Modifier within the same package as well as from subclasses.");
+	}
+  public int num;
+  public void display() {
+	  System.out.println("We can access public modifier within another package,another class also.");
+	  }
+ 
+ 
+   }
+  
+
+
+
---
+++ git11Questions/AccessMod2A.java
@@ -0,0 +1,15 @@
+package git11Questions;
+
+public class AccessMod2A extends AccessMod2{
+	public static void main (String[] args) {
+		AccessMod2A m=new AccessMod2A();
+		m.show();
+		
+		AccessMod2 a=new AccessMod2();
+		a.num=4;
+		a.display();
+	}
+}
+	   
+
+
